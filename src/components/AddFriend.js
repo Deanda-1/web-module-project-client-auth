0a06d6c4a7516d32080e1861a4ed6a5f5aa6ef7c
DJ1 <jimmynoble604@gmail.com>
2023-08-24 13:43:44 -0400

    AddFriend.js

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,24 +1,54 @@
-import React from 'react';
-
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Axios from 'axios';
 
 
 
 
 const AddFriend = () => {
+    const { push } = useHistory();
+    const [form, setForm] = useState({
+        name: '',
+        age: '',
+        email: ''
+    });
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]:e.target.value 
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+        Axios.post('http://localhost5000/api/friends', form, {
+            Headers: {
+                authorization: token
+            }                                
+        })
+        .then(resp => {
+            push('/friends');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
     return (<div>
         <h2>AddFriend</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
-                <label htmlFor="username">Username:</label>
-                <input id="username" />
+                <label htmlFor="name">Name:</label>
+                <input onChange={handleChange} name="name" id="name" />
             </div>
             <div>
                 <label htmlFor="age">Age:</label>
-                <input id="age" />
+                <input onChange={handleChange} name="age" id="age" />
             </div>
             <div>
                 <label htmlFor="email">Email:</label>
-                <input id="email" />
+                <input onChange={handleChange} name="email" id="email" />
             </div>
         </form>
         </div>)
