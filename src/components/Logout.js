e21b937e401d02efe025b10ff898b7bab60b6b7b
DJ1 <jimmynoble604@gmail.com>
2023-08-24 14:54:52 -0400

    Logout,js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Axios from 'axios';
 
 
@@ -8,10 +9,17 @@ import Axios from 'axios';
 
 
 const Logout = () => {
+    const { push } = useHistory();
     useEffect(() => {
-        Axios.post('http://lodalhost:5000/api/logout')
+        const token = localStorage.getItem("token");
+        Axios.post('http://lodalhost:5000/api/logout', {}, {
+            headers: {
+                authorization: token
+            }
+        })
         .then(resp => {
-            console.log(resp);
+            localStorage.removeItem("token");
+            push('/login');
         })
         .catch(err => {
             console.log(err);
