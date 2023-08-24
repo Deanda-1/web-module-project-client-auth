210c7cb24c55f0e0b4711ca846264969fa2c496a
DJ1 <jimmynoble604@gmail.com>
2023-08-24 15:36:19 -0400

    Fixed 1 error

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 
-const privateRoute = ({component:Component ...rest}) => {
+const privateRoute = ({component:Component, ...rest}) => {
     return <Route {...rest} render={(props) => {
         if (localStorage.getItem('token')) {
             return<Component {...props} />;
